bab5320d7e0a0d105c481e595d21a63f409a2809
Olivier Vignaud <ovi@ubiqube.com>
2023-08-25 23:09:19 +0200

    3.3.1: Fix Create vnf request.

--- 3.5.1/model/src/main/java/com/ubiqube/etsi/mano/v351/services/HttpGateway351.java
+++ 3.5.1/model/src/main/java/com/ubiqube/etsi/mano/v351/services/HttpGateway351.java
@@ -227,8 +227,10 @@ public class HttpGateway351 extends AbstractHttpGateway {
 	}
 
 	@Override
-	public Class<?> createVnfPackageRequest(final Map<String, String> userDefinedData) {
-		return CreateVnfPkgInfoRequest.class;
+	public Object createVnfPackageRequest(final Map<String, String> userDefinedData) {
+		final CreateVnfPkgInfoRequest req = new CreateVnfPkgInfoRequest();
+		req.setUserDefinedData(userDefinedData);
+		return req;
 	}
 
 	@Override
