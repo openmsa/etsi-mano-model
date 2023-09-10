ffe9209c425347a0b5961656c79d919728ac4e10
Olivier Vignaud <ovi@ubiqube.com>
2023-09-10 10:37:39 +0200

    3.3.1: Fix package reparenting.

--- 3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnfind/VnfIndSubscriptions331Sol003Controller.java
+++ 3.3.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v331/controller/vnfind/VnfIndSubscriptions331Sol003Controller.java
@@ -26,11 +26,11 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ubiqube.etsi.mano.controller.SubscriptionFrontController;
+import com.ubiqube.etsi.mano.dao.subscription.SubscriptionType;
 import com.ubiqube.etsi.mano.em.v331.model.vnfind.VnfIndicatorSubscription;
 import com.ubiqube.etsi.mano.em.v331.model.vnfind.VnfIndicatorSubscriptionLinks;
 import com.ubiqube.etsi.mano.em.v331.model.vnfind.VnfIndicatorSubscriptionRequest;
 import com.ubiqube.etsi.mano.em.v331.model.vnflcm.Link;
-import com.ubiqube.etsi.mano.service.event.model.SubscriptionType;
 
 import jakarta.validation.Valid;
 
