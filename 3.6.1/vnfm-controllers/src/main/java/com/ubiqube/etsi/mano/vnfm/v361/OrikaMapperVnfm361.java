e3275169cc4b2e96b43b1b26cfdbb66afef2c1de
Olivier Vignaud <ovi@ubiqube.com>
2024-03-16 17:36:11 +0100

    🩹 3.6.1: VNFM Fix grant response mapping.

--- 3.6.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v361/OrikaMapperVnfm361.java
+++ 3.6.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v361/OrikaMapperVnfm361.java
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Service;
 import com.ubiqube.etsi.mano.dao.mano.ExtManagedVirtualLinkDataEntity;
 import com.ubiqube.etsi.mano.dao.mano.ExtVirtualLinkDataEntity;
 import com.ubiqube.etsi.mano.dao.mano.GrantInformationExt;
+import com.ubiqube.etsi.mano.dao.mano.GrantResponse;
 import com.ubiqube.etsi.mano.dao.mano.VnfCompute;
 import com.ubiqube.etsi.mano.dao.mano.VnfExtCpDataEntity;
 import com.ubiqube.etsi.mano.dao.mano.VnfInstance;
@@ -71,6 +72,7 @@ import com.ubiqube.etsi.mano.nfvo.v361.model.vnfsnapshotpkgm.Checksum;
 import com.ubiqube.etsi.mano.service.auth.model.AuthParamOauth2;
 import com.ubiqube.etsi.mano.service.auth.model.AuthentificationInformations;
 import com.ubiqube.etsi.mano.service.event.model.Subscription;
+import com.ubiqube.etsi.mano.vnfm.v361.model.grant.Grant;
 import com.ubiqube.etsi.mano.vnfm.v361.model.grant.ResourceDefinition;
 import com.ubiqube.orika.OrikaMapperFactoryConfigurer;
 
@@ -321,6 +323,13 @@ public class OrikaMapperVnfm361 implements OrikaMapperFactoryConfigurer {
 		 */
 		orikaMapperFactory.classMap(Alarm.class, Alarms.class)
 				.field("isRootCause", "rootCause")
+				.byDefault()
+				.register();
+		orikaMapperFactory.classMap(Grant.class, GrantResponse.class)
+				.field("links.vnfInstance.href", "instanceLink")
+				.field("links.vnfLcmOpOcc.href", "lcmLink")
+				.field("vimConnectionInfo{key}", "vimConnections{vimId}")
+				.field("vimConnectionInfo{value}", "vimConnections{}")
 				.byDefault()
 				.register();
 		final ConverterFactory converterFactory = orikaMapperFactory.getConverterFactory();
