9bef7aa94a60751c4d3b430155e22d1b9c8c8436
Olivier Vignaud <ovi@ubiqube.com>
2024-04-07 16:07:06 +0200

    ✅ test-helper: Split classes to use multiple mapper.

---
+++ test-helper/src/main/java/com/ubiqube/etsi/mano/test/MapstructTestHelper.java
@@ -0,0 +1,44 @@
+/**
+ *     Copyright (C) 2019-2024 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.test;
+
+import java.beans.IntrospectionException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Set;
+import java.util.function.Function;
+
+import uk.co.jemos.podam.api.PodamFactory;
+
+public class MapstructTestHelper extends AssertNull {
+
+	private final PodamFactory podam;
+
+	public MapstructTestHelper() {
+		this.podam = TestUtils.createPodam();
+	}
+
+	protected <T, R> void doTest(final Class<T> clazz, final Function<T, R> in, final Function<R, T> out) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, IntrospectionException {
+		final T obj = podam.manufacturePojo(clazz);
+		final R tmp = in.apply(obj);
+		final T r2 = out.apply(tmp);
+		final Deque<String> stack = new ArrayDeque<>();
+		assertFullEqual(obj, r2, Set.of("getLinks"), stack);
+	}
+
+}
---
+++ test-helper/src/main/java/com/ubiqube/etsi/mano/test/TestUtils.java
@@ -0,0 +1,55 @@
+/**
+ *     Copyright (C) 2019-2024 Ubiqube.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.ubiqube.etsi.mano.test;
+
+import com.ubiqube.etsi.mano.mapper.OffsetDateTimeToDateConverter;
+import com.ubiqube.etsi.mano.mapper.OrikaFilterMapper;
+import com.ubiqube.etsi.mano.mapper.UuidConverter;
+import com.ubiqube.orika.OrikaMapperFactoryConfigurer;
+
+import ma.glasnost.orika.OrikaSystemProperties;
+import ma.glasnost.orika.converter.ConverterFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
+import ma.glasnost.orika.impl.generator.EclipseJdtCompilerStrategy;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+public class TestUtils {
+	private TestUtils() {
+		// Nothing.
+	}
+
+	public static PodamFactory createPodam() {
+		final PodamFactoryImpl podam = new PodamFactoryImpl();
+		podam.getStrategy().addOrReplaceTypeManufacturer(String.class, new UUIDManufacturer());
+		podam.getStrategy().setDefaultNumberOfCollectionElements(1);
+		return podam;
+	}
+
+	public static DefaultMapperFactory createOrika(final OrikaMapperFactoryConfigurer orikaMapperFactoryConfigurer) {
+		System.setProperty(OrikaSystemProperties.COMPILER_STRATEGY, EclipseJdtCompilerStrategy.class.getName());
+		System.setProperty(OrikaSystemProperties.WRITE_SOURCE_FILES, "true");
+		System.setProperty(OrikaSystemProperties.WRITE_SOURCE_FILES_TO_PATH, "/tmp/orika-test");
+		final DefaultMapperFactory mapperFactory = new DefaultMapperFactory.Builder().compilerStrategy(new EclipseJdtCompilerStrategy()).build();
+		orikaMapperFactoryConfigurer.configure(mapperFactory);
+		final ConverterFactory converterFactory = mapperFactory.getConverterFactory();
+		converterFactory.registerConverter("filterConverter", new OrikaFilterMapper());
+		converterFactory.registerConverter(new UuidConverter());
+		converterFactory.registerConverter(new OffsetDateTimeToDateConverter());
+		return mapperFactory;
+	}
+}
