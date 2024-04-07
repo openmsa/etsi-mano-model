f6f5cb01a7f6b55e944a30eca639db50a0ddc735
Olivier Vignaud <ovi@ubiqube.com>
2024-04-07 20:04:46 +0200

    🎨 4.5.1: Add Vnf lcm controllers.

---
+++ 4.5.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/vnfm/v451/controller/vnflcm/VnfLcmSubscriptions451Sol003Controller.java
@@ -0,0 +1,34 @@
+package com.ubiqube.etsi.mano.vnfm.v451.controller.vnflcm;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class VnfLcmSubscriptions451Sol003Controller implements VnfLcmSubscriptions451Sol003Api {
+
+	private final ObjectMapper objectMapper;
+
+	private final HttpServletRequest request;
+
+	@org.springframework.beans.factory.annotation.Autowired
+	public VnfLcmSubscriptions451Sol003Controller(final ObjectMapper objectMapper, final HttpServletRequest request) {
+		this.objectMapper = objectMapper;
+		this.request = request;
+	}
+
+	@Override
+	public Optional<ObjectMapper> getObjectMapper() {
+		return Optional.ofNullable(objectMapper);
+	}
+
+	@Override
+	public Optional<HttpServletRequest> getRequest() {
+		return Optional.ofNullable(request);
+	}
+
+}
