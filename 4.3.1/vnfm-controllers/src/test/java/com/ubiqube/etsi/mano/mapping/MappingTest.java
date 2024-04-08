9be171c325b7e1731404aa2609534d67a56dba65
Olivier Vignaud <ovi@ubiqube.com>
2024-04-08 17:48:23 +0200

    ✅ 4.3.1: Fix test units.

--- 4.3.1/vnfm-controllers/src/test/java/com/ubiqube/etsi/mano/mapping/MappingTest.java
+++ 4.3.1/vnfm-controllers/src/test/java/com/ubiqube/etsi/mano/mapping/MappingTest.java
@@ -26,12 +26,12 @@ import org.junit.jupiter.api.Test;
 import com.ubiqube.etsi.mano.dao.mano.GrantResponse;
 import com.ubiqube.etsi.mano.dao.mano.VnfPackage;
 import com.ubiqube.etsi.mano.dao.mano.v2.VnfBlueprint;
+import com.ubiqube.etsi.mano.em.v431.model.vnflcm.VnfInstance;
+import com.ubiqube.etsi.mano.em.v431.model.vnflcm.VnfLcmOpOcc;
 import com.ubiqube.etsi.mano.em.v431.service.OrikaMapperVnfm431;
+import com.ubiqube.etsi.mano.nfvo.v431.model.vnf.VnfPkgInfo;
 import com.ubiqube.etsi.mano.test.TestHelper;
 import com.ubiqube.etsi.mano.vnfm.v431.model.grant.GrantRequest;
-import com.ubiqube.etsi.mano.vnfm.v431.model.vnf.VnfPkgInfo;
-import com.ubiqube.etsi.mano.vnfm.v431.model.vnflcm.VnfInstance;
-import com.ubiqube.etsi.mano.vnfm.v431.model.vnflcm.VnfLcmOpOcc;
 
 /**
  *
