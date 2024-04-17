fa734bdfeca052d844a0089b13c44b51b722cc8b
Olivier Vignaud <ovi@ubiqube.com>
2024-04-17 10:12:07 +0200

    🌱 4.5.1-vnfm: Add vnflcm controllers.

--- 4.5.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/v451/controller/em/vnflcm/VnfLcmSubscriptions451Sol002Controller.java
+++ 4.5.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/v451/controller/em/vnflcm/VnfLcmSubscriptions451Sol002Controller.java
@@ -16,32 +16,81 @@
  */
 package com.ubiqube.etsi.mano.v451.controller.em.vnflcm;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static com.ubiqube.etsi.mano.uri.ManoWebMvcLinkBuilder.linkTo;
+import static com.ubiqube.etsi.mano.uri.ManoWebMvcLinkBuilder.methodOn;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RestController;
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.Optional;
+
+import com.ubiqube.etsi.mano.controller.subscription.ApiAndType;
+import com.ubiqube.etsi.mano.dao.subscription.SubscriptionType;
+import com.ubiqube.etsi.mano.service.auth.model.ApiTypesEnum;
+import com.ubiqube.etsi.mano.service.event.model.Subscription;
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.LccnSubscription;
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.LccnSubscriptionLinks;
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.LccnSubscriptionRequest;
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.LifecycleChangeNotificationsFilter;
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.Link;
+import com.ubiqube.etsi.mano.v451.service.SubscriptionLinkable451Vnfm;
+import com.ubiqube.etsi.mano.v451.service.mapping.subscription.LccnSubscription451Mapping;
+import com.ubiqube.etsi.mano.vnfm.fc.vnflcm.VnfLcmSubscriptionFrontController;
+
+import jakarta.validation.Valid;
 
 @RestController
-public class VnfLcmSubscriptions451Sol002Controller implements VnfLcmSubscriptions451Sol002Api {
+public class VnfLcmSubscriptions451Sol002Controller implements VnfLcmSubscriptions451Sol002Api, SubscriptionLinkable451Vnfm {
+	private final VnfLcmSubscriptionFrontController frontController;
+	private final LccnSubscription451Mapping mapper;
 
-    private final ObjectMapper objectMapper;
+	public VnfLcmSubscriptions451Sol002Controller(final VnfLcmSubscriptionFrontController frontController, final LccnSubscription451Mapping mapper) {
+		this.frontController = frontController;
+		this.mapper = mapper;
+	}
 
-    private final HttpServletRequest request;
+	@Override
+	public ResponseEntity<List<LccnSubscription>> subscriptionsGet(final MultiValueMap<String, String> requestParams, @Valid final String nextpageOpaqueMarker) {
+		return frontController.search(requestParams, nextpageOpaqueMarker, x -> mapper.map(x, LifecycleChangeNotificationsFilter.class), VnfLcmSubscriptions451Sol002Controller::makeLinks);
+	}
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public VnfLcmSubscriptions451Sol002Controller(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	@Override
+	public ResponseEntity<LccnSubscription> subscriptionsPost(@Valid final LccnSubscriptionRequest body) {
+		final Subscription req = mapper.map(body);
+		return frontController.create(req, x -> mapper.map(x, LifecycleChangeNotificationsFilter.class), VnfLcmSubscriptions451Sol002Api.class, VnfLcmSubscriptions451Sol002Controller::makeLinks, VnfLcmSubscriptions451Sol002Controller::getSelfLink);
+	}
 
-    @Override
-    public Optional<ObjectMapper> getObjectMapper() {
-        return Optional.ofNullable(objectMapper);
-    }
+	@Override
+	public ResponseEntity<Void> subscriptionsSubscriptionIdDelete(final String subscriptionId) {
+		return frontController.deleteById(subscriptionId);
+	}
 
-    @Override
-    public Optional<HttpServletRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
+	@Override
+	public ResponseEntity<LccnSubscription> subscriptionsSubscriptionIdGet(final String subscriptionId) {
+		return frontController.findById(subscriptionId, x -> mapper.map(x, LifecycleChangeNotificationsFilter.class), VnfLcmSubscriptions451Sol002Controller::makeLinks);
+	}
+
+	private static void makeLinks(final LccnSubscription subscription) {
+		final LccnSubscriptionLinks links = new LccnSubscriptionLinks();
+		final Link link = new Link();
+		link.setHref(linkTo(methodOn(VnfLcmSubscriptions451Sol002Api.class).subscriptionsSubscriptionIdGet(subscription.getId())).withSelfRel().getHref());
+		links.setSelf(link);
+		subscription.setLinks(links);
+	}
+
+	private static String getSelfLink(final LccnSubscription subscription) {
+		return linkTo(methodOn(VnfLcmSubscriptions451Sol002Api.class).subscriptionsSubscriptionIdGet(subscription.getId())).withSelfRel().getHref();
+	}
+
+	@Override
+	public String makeSelfLink(final String id) {
+		return linkTo(methodOn(VnfLcmSubscriptions451Sol002Api.class).subscriptionsSubscriptionIdGet(id)).withSelfRel().getHref();
+	}
+
+	@Override
+	public ApiAndType getApiAndType() {
+		return ApiAndType.of(ApiTypesEnum.SOL002, SubscriptionType.VNFLCM);
+	}
 
 }
--- 4.5.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/v451/controller/em/vnflcm/VnfSnapshots451Sol002Controller.java
+++ 4.5.1/vnfm-controllers/src/main/java/com/ubiqube/etsi/mano/v451/controller/em/vnflcm/VnfSnapshots451Sol002Controller.java
@@ -16,32 +16,41 @@
  */
 package com.ubiqube.etsi.mano.v451.controller.em.vnflcm;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import jakarta.servlet.http.HttpServletRequest;
-import java.util.Optional;
+
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.CreateVnfSnapshotInfoRequest;
+import com.ubiqube.etsi.mano.v451.model.em.vnflcm.VnfSnapshotInfo;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class VnfSnapshots451Sol002Controller implements VnfSnapshots451Sol002Api {
 
-    private final ObjectMapper objectMapper;
+	@Override
+	public ResponseEntity<List<VnfSnapshotInfo>> vnfSnapshotsGet(@Valid final String filter, @Valid final String allFields, @Valid final String fields, @Valid final String excludeFields, @Valid final String excludeDefault, @Valid final String nextpageOpaqueMarker) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    private final HttpServletRequest request;
+	@Override
+	public ResponseEntity<VnfSnapshotInfo> vnfSnapshotsPost(@Valid final CreateVnfSnapshotInfoRequest body) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @org.springframework.beans.factory.annotation.Autowired
-    public VnfSnapshots451Sol002Controller(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
+	@Override
+	public ResponseEntity<Void> vnfSnapshotsVnfSnapshotInfoIdDelete(final String vnfSnapshotInfoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public Optional<ObjectMapper> getObjectMapper() {
-        return Optional.ofNullable(objectMapper);
-    }
-
-    @Override
-    public Optional<HttpServletRequest> getRequest() {
-        return Optional.ofNullable(request);
-    }
+	@Override
+	public ResponseEntity<VnfSnapshotInfo> vnfSnapshotsVnfSnapshotInfoIdGet(final String vnfSnapshotInfoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
