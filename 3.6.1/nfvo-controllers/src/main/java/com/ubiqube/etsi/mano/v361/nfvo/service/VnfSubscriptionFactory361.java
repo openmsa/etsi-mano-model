fe5de9b862bde63d2e3b4d1a0f46ae7dbee5f6ce
Olivier Vignaud <ovi@ubiqube.com>
2024-04-19 15:11:08 +0200

    🐛 3.6.1: Fix id field on `VnfPackageChangeNotification`.

--- 3.6.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/v361/nfvo/service/VnfSubscriptionFactory361.java
+++ 3.6.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/v361/nfvo/service/VnfSubscriptionFactory361.java
@@ -19,13 +19,13 @@ package com.ubiqube.etsi.mano.v361.nfvo.service;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
-import jakarta.annotation.Nonnull;
-
 import com.ubiqube.etsi.mano.v361.model.nfvo.vnf.PackageChangeType;
 import com.ubiqube.etsi.mano.v361.model.nfvo.vnf.PackageOperationalStateType;
 import com.ubiqube.etsi.mano.v361.model.nfvo.vnf.VnfPackageChangeNotification;
 import com.ubiqube.etsi.mano.v361.model.nfvo.vnf.VnfPackageOnboardingNotification;
 import com.ubiqube.etsi.mano.v361.service.Linkable;
+
+import jakarta.annotation.Nonnull;
 
 /**
  *
@@ -41,6 +41,7 @@ public class VnfSubscriptionFactory361 {
 	@Nonnull
 	public static VnfPackageChangeNotification createVnfPackageChangeNotification(final boolean deleted, final UUID subscriptionId, @Nonnull final UUID vnfPkgId, final String vnfdId, final UUID eventId, final PackageOperationalStateType state, final Linkable links) {
 		final VnfPackageChangeNotification ret = new VnfPackageChangeNotification();
+		ret.setId(eventId.toString());
 		if (deleted) {
 			ret.setChangeType(PackageChangeType.PKG_DELETE);
 		} else {
