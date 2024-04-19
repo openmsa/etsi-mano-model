15c0d229675d80028cb13bd0768e3d62a2753cfc
Olivier Vignaud <ovi@ubiqube.com>
2024-04-19 14:04:05 +0200

    🐛 2.7.1: Fix get vnfPkgInfo list.

--- 2.7.1/model/src/main/java/com/ubiqube/etsi/mano/v271/services/HttpGateway271.java
+++ 2.7.1/model/src/main/java/com/ubiqube/etsi/mano/v271/services/HttpGateway271.java
@@ -204,7 +204,7 @@ public class HttpGateway271 extends AbstractHttpGateway {
 
 	@Override
 	public ParameterizedTypeReference<List<Class<?>>> getVnfPackageClassList() {
-		final ParameterizedTypeReference<List<VnfPackage>> res = new ParameterizedTypeReference<>() {
+		final ParameterizedTypeReference<List<VnfPkgInfo>> res = new ParameterizedTypeReference<>() {
 			// Nothing.
 		};
 		return (ParameterizedTypeReference<List<Class<?>>>) (Object) res;
