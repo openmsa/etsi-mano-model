e5ec0965a9149c38f3ab048853509045f7f2192b
Olivier Vignaud <ovi@ubiqube.com>
2024-04-20 12:33:38 +0200

    🐛 4.3.1: Fix controller with missing `@RequestParam`.

--- 4.3.1/etsi-nfvem-431-controllers/src/main/java/com/ubiqube/etsi/mano/v431/controller/nfvem/nfvmanologm/LogJobs431Sol009Api.java
+++ 4.3.1/etsi-nfvem-431-controllers/src/main/java/com/ubiqube/etsi/mano/v431/controller/nfvem/nfvmanologm/LogJobs431Sol009Api.java
@@ -68,7 +68,7 @@ public interface LogJobs431Sol009Api {
 			@ApiResponse(responseCode = "504", description = "504 GATEWAY TIMEOUT If the API producer encounters a timeout while waiting for a response from an upstream server (i.e. a server that the API producer communicates with when fulfilling a request), it should respond with this response code. ", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProblemDetail.class))) })
 	@GetMapping(value = "/log_jobs", produces = { "application/json" })
 	ResponseEntity<String> logJobsGet(
-			MultiValueMap<String, String> requestParams,
+			@RequestParam MultiValueMap<String, String> requestParams,
 			@Parameter(in = ParameterIn.QUERY, description = "Marker to obtain the next page of a paged response. Shall be supported by  the NFV-MANO functional entity if the entity supports alternative 2 (paging)  according to clause 5.4.2.1 of ETSI GS NFV-SOL 013 for this resource. ", schema = @Schema()) @Valid @RequestParam(value = "nextpage_opaque_marker", required = false) final String nextpageOpaqueMarker);
 
 	@Operation(summary = "", description = "The POST method requests to compile the logged data into a file and create an associated log report. See clause 8.5.6.3.1.", tags = {})
