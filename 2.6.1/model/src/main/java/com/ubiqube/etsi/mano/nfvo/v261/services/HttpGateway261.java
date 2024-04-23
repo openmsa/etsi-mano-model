c9fb0f43399afa2ce33fb743fed8cd57984013fd
Olivier Vignaud <ovi@ubiqube.com>
2024-04-23 11:22:44 +0200

    🐛 2.6.1: Fix HttpGateway for vnf package creation.

--- 2.6.1/model/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/services/HttpGateway261.java
+++ 2.6.1/model/src/main/java/com/ubiqube/etsi/mano/nfvo/v261/services/HttpGateway261.java
@@ -219,8 +219,10 @@ public class HttpGateway261 extends AbstractHttpGateway {
 	}
 
 	@Override
-	public Class<?> createVnfPackageRequest(final Map<String, String> userDefinedData) {
-		return CreateVnfPkgInfoRequest.class;
+	public Object createVnfPackageRequest(final Map<String, String> userDefinedData) {
+		final CreateVnfPkgInfoRequest req = new CreateVnfPkgInfoRequest();
+		req.setUserDefinedData(userDefinedData);
+		return req;
 	}
 
 	@Override
