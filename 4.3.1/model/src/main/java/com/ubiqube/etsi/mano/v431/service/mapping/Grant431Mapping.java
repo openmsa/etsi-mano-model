19f33e0820f9f70bcd6ed21dfd7cdc15f998c7c9
Olivier Vignaud <ovi@ubiqube.com>
2024-05-02 02:24:58 -0400

    🩹 4.3.1-nfvo; Fix Grant link / mapping.

--- 4.3.1/model/src/main/java/com/ubiqube/etsi/mano/v431/service/mapping/Grant431Mapping.java
+++ 4.3.1/model/src/main/java/com/ubiqube/etsi/mano/v431/service/mapping/Grant431Mapping.java
@@ -79,7 +79,8 @@ public interface Grant431Mapping extends VimConnectionInfo431Mapping, Connectivi
 	@Mapping(target = "id", ignore = true)
 	ConstraintResourceRef map(com.ubiqube.etsi.mano.v431.model.vnfm.grant.ConstraintResourceRef o);
 
-	@Mapping(target = "links", ignore = true)
+	@Mapping(target = "links.vnfInstance.href", source = "instanceLink")
+	@Mapping(target = "links.vnfLcmOpOcc.href", source = "lcmLink")
 	@Mapping(target = "vimConnectionInfo", source = "vimConnections")
 	Grant map(GrantResponse grantResponse);
 
