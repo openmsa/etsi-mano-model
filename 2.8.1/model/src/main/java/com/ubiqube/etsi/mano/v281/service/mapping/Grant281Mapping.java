8efee9db21b54d4b021bec7c525879bca67c5969
Olivier Vignaud <ovi@ubiqube.com>
2024-05-02 02:24:28 -0400

    🩹 2.8.1-nfvo; Fix Grant link / mapping.

--- 2.8.1/model/src/main/java/com/ubiqube/etsi/mano/v281/service/mapping/Grant281Mapping.java
+++ 2.8.1/model/src/main/java/com/ubiqube/etsi/mano/v281/service/mapping/Grant281Mapping.java
@@ -79,7 +79,8 @@ public interface Grant281Mapping extends VimConnectionInfo281Mapping, Connectivi
 	@Mapping(target = "id", ignore = true)
 	ConstraintResourceRef map(com.ubiqube.etsi.mano.v281.model.vnfm.grant.ConstraintResourceRef o);
 
-	@Mapping(target = "links", ignore = true)
+	@Mapping(target = "links.vnfInstance.href", source = "instanceLink")
+	@Mapping(target = "links.vnfLcmOpOcc.href", source = "lcmLink")
 	Grant map(GrantResponse grantResponse);
 
 	@Mapping(target = "cirConnectionInfo", ignore = true)
--- 2.8.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/v281/nfvo/controller/vnfm/grant/Grants281Sol003Controller.java
+++ 2.8.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/v281/nfvo/controller/vnfm/grant/Grants281Sol003Controller.java
@@ -19,6 +19,8 @@ package com.ubiqube.etsi.mano.v281.nfvo.controller.vnfm.grant;
 import static com.ubiqube.etsi.mano.uri.ManoWebMvcLinkBuilder.linkTo;
 import static com.ubiqube.etsi.mano.uri.ManoWebMvcLinkBuilder.methodOn;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Conditional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,7 @@ import com.ubiqube.etsi.mano.controller.lcmgrant.LcmGrantsFrontController;
 import com.ubiqube.etsi.mano.dao.mano.GrantResponse;
 import com.ubiqube.etsi.mano.v281.model.em.vnflcm.Link;
 import com.ubiqube.etsi.mano.v281.model.vnfm.grant.Grant;
+import com.ubiqube.etsi.mano.v281.model.vnfm.grant.GrantLinks;
 import com.ubiqube.etsi.mano.v281.model.vnfm.grant.GrantRequest;
 import com.ubiqube.etsi.mano.v281.service.mapping.Grant281Mapping;
 
@@ -61,9 +64,11 @@ public class Grants281Sol003Controller implements Grants281Sol003Api {
 	}
 
 	private static void makeSelfLinks(final Grant jsonGrant) {
+		final GrantLinks grantLinks = Optional.ofNullable(jsonGrant.getLinks()).orElseGet(GrantLinks::new);
 		final Link link = new Link();
 		link.setHref(linkTo(methodOn(Grants281Sol003Api.class).grantsGrantIdGet(jsonGrant.getId())).withSelfRel().getHref());
-		jsonGrant.getLinks().setSelf(link);
+		grantLinks.setSelf(link);
+		jsonGrant.setLinks(grantLinks);
 	}
 
 	private static String getSelfLink(final Grant grant) {
