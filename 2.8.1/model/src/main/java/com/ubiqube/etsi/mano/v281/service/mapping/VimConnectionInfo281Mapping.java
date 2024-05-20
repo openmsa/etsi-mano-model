2209f73257d13ac25fbc3643c013b2526b1bd4a0
Olivier Vignaud <ovi@ubiqube.com>
2024-05-20 10:08:16 +0200

    👽 2.8.1: Change connection information object.

--- 2.8.1/model/src/main/java/com/ubiqube/etsi/mano/v281/service/mapping/VimConnectionInfo281Mapping.java
+++ 2.8.1/model/src/main/java/com/ubiqube/etsi/mano/v281/service/mapping/VimConnectionInfo281Mapping.java
@@ -25,12 +25,13 @@ import org.mapstruct.Mapping;
 
 import com.ubiqube.etsi.mano.dao.mano.cnf.ConnectionInformation;
 import com.ubiqube.etsi.mano.dao.mano.vim.VimConnectionInformation;
+import com.ubiqube.etsi.mano.service.mapping.ConnectionMapping;
 import com.ubiqube.etsi.mano.v281.model.vnfm.grant.VimConnectionInfo;
 
 import jakarta.annotation.Nullable;
 
 @Mapper
-public interface VimConnectionInfo281Mapping {
+public interface VimConnectionInfo281Mapping extends ConnectionMapping {
 	@Mapping(target = "audit", ignore = true)
 	@Mapping(target = "cnfInfo", ignore = true)
 	@Mapping(target = "id", ignore = true)
