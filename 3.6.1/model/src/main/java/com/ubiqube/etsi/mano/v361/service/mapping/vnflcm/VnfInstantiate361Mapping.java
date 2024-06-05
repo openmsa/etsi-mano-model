ae71bbbf17af9476709e91ad38c94379013fe3f9
Olivier Vignaud <ovi@ubiqube.com>
2024-06-05 17:13:02 +0200

    🐛 3.6.1: Fix connection mappng.

--- 3.6.1/model/src/main/java/com/ubiqube/etsi/mano/v361/service/mapping/vnflcm/VnfInstantiate361Mapping.java
+++ 3.6.1/model/src/main/java/com/ubiqube/etsi/mano/v361/service/mapping/vnflcm/VnfInstantiate361Mapping.java
@@ -92,16 +92,6 @@ public interface VnfInstantiate361Mapping extends Connectivity361Mapping, VimCon
 		return value.values().stream().map(this::map).toList();
 	}
 
-	@Override
-	@Mapping(target = "audit", ignore = true)
-	@Mapping(target = "cnfInfo", ignore = true)
-	@Mapping(target = "id", ignore = true)
-	@Mapping(target = "jujuInfo", ignore = true)
-	@Mapping(target = "tenantId", ignore = true)
-	@Mapping(target = "version", ignore = true)
-	@Mapping(target = "vimCapabilities", ignore = true)
-	VimConnectionInformation map(VimConnectionInfo o);
-
 	@Mapping(target = "additionalParams", ignore = true)
 	@Mapping(target = "extensions", ignore = true)
 	@Mapping(target = "vnfConfigurableProperties", ignore = true)
