46510e45d6021899e2351a63edcd16f00853f130
Olivier Vignaud <ovi@ubiqube.com>
2024-06-08 17:38:44 +0200

    👽️ 3.5.1: Fix removal of `OSCONTAINER`.

--- 3.5.1/model/src/main/java/com/ubiqube/etsi/mano/v351/service/mapping/Grant351Mapping.java
+++ 3.5.1/model/src/main/java/com/ubiqube/etsi/mano/v351/service/mapping/Grant351Mapping.java
@@ -177,7 +177,6 @@ public interface Grant351Mapping extends VimConnectionInfo351Mapping, Connectivi
 		return value.iterator().next();
 	}
 
-	@ValueMapping(source = "OSCONTAINER", target = MappingConstants.THROW_EXCEPTION)
 	@ValueMapping(source = "VIRTUALCP", target = MappingConstants.THROW_EXCEPTION)
 	@ValueMapping(source = "AFFINITY_RULE", target = MappingConstants.THROW_EXCEPTION)
 	@ValueMapping(source = "DNSHOST", target = MappingConstants.THROW_EXCEPTION)
