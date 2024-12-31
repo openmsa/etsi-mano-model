d8bc52d71e61a76662bd785d4ce2f550ca4196c0
Olivier Vignaud <ovi@ubiqube.com>
2024-12-31 12:55:38 +0100

    🐛 4.5.1: Fix NsLcmOpOccs controller.

--- 4.5.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/v451/nfvo/controller/nfvo/nslcm/NsLcmOpOccs451Sol005Controller.java
+++ 4.5.1/nfvo-controllers/src/main/java/com/ubiqube/etsi/mano/v451/nfvo/controller/nfvo/nslcm/NsLcmOpOccs451Sol005Controller.java
@@ -1,18 +1,18 @@
 /**
- *     Copyright (C) 2019-2024 Ubiqube.
+ * Copyright (C) 2019-2024 Ubiqube.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package com.ubiqube.etsi.mano.v451.nfvo.controller.nfvo.nslcm;
 
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ubiqube.etsi.mano.controller.nslcm.NsLcmGenericFrontController;
 import com.ubiqube.etsi.mano.v451.model.em.vnflcm.Link;
-import com.ubiqube.etsi.mano.v451.model.nfvo.nslcm.NsInstance;
 import com.ubiqube.etsi.mano.v451.model.nfvo.nslcm.NsLcmInfoModificationRequest;
 import com.ubiqube.etsi.mano.v451.model.nfvo.nslcm.NsLcmOpOcc;
 import com.ubiqube.etsi.mano.v451.model.nfvo.nslcm.NsLcmOpOccLinks;
@@ -46,7 +45,7 @@ public class NsLcmOpOccs451Sol005Controller implements NsLcmOpOccs451Sol005Api {
 
 	@Override
 	public ResponseEntity<String> nsLcmOpOccsGet(final MultiValueMap<String, String> requestParams, final String nextpageOpaqueMarker) {
-		return nsLcmGenericFrontController.search(requestParams, x -> mapper.map(x), nextpageOpaqueMarker, NsLcmOpOccs451Sol005Controller::makeLinks, NsInstance.class);
+		return nsLcmGenericFrontController.search(requestParams, x -> mapper.map(x), nextpageOpaqueMarker, NsLcmOpOccs451Sol005Controller::makeLinks, NsLcmOpOcc.class);
 	}
 
 	@Override
