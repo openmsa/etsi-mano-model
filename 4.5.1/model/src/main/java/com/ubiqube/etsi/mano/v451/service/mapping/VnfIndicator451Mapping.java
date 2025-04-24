fda8c821c4fbf399cccbbc62ce6e342bdfcf62cc
Olivier Vignaud <ovi@ubiqube.com>
2025-04-24 09:37:00 +0200

    🐛 4.5.1: Fix vnf indicator mapping.

--- 4.5.1/model/src/main/java/com/ubiqube/etsi/mano/v451/service/mapping/VnfIndicator451Mapping.java
+++ 4.5.1/model/src/main/java/com/ubiqube/etsi/mano/v451/service/mapping/VnfIndicator451Mapping.java
@@ -38,5 +38,6 @@ public interface VnfIndicator451Mapping {
 
 	@Mapping(target = "links", ignore = true)
 	@Mapping(target = "value", ignore = true)
+	@Mapping(target = "name", source = "toscaName")
 	com.ubiqube.etsi.mano.v451.model.em.vnfind.VnfIndicator map(VnfIndicator o);
 }
